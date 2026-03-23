d452605135dc507dcb86cd9862c9e668372e3aa8
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-03-23 21:27:14 +0530

    update project

--- src/components/product.js
+++ src/components/product.js
@@ -5,16 +5,16 @@ import three from "../img/three.jpg"
 
 function Product() {
   return (
-    <div class="products">
-      <div class="box">
+    <div className="products">
+      <div className="box">
         <img src={one} alt="hero" />
         <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
       </div>
-      <div class="box">
+      <div className="box">
         <img src={two} alt="villain" />
         <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>
       </div>
-      <div class="box">
+      <div className="box">
         <img src={three} alt="Designs Club" />
         <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
       </div>
